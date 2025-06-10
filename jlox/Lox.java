95027c0473c37fd62290e3868da6fe1d8b5825b9
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2025-06-10 22:36:04 +0530

    scanner functionality

---
+++ jlox/Lox.java
@@ -0,0 +1,67 @@
+package jlox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox {
+    static boolean hadError = false;
+    public static void main(String[] args) throws IOException {
+        if(args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if(args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+        if(hadError) System.exit(65);
+    }
+
+    private static void runPrompt() {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print("> ");
+            try {
+                String line = reader.readLine();
+                if(line == null) break;
+                run(line);
+                hadError = false; // reset hadError. If user makes mistake in interactive session, it should not kill the entire session
+            } catch (IOException exception) {
+                System.err.println("Error reading input: " + exception.getMessage());
+                System.exit(1);
+            } catch (Exception exception) {
+                System.err.println("Error: " + exception.getMessage());
+            }
+        }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for(Token token : tokens) {
+            System.out.println(token);
+        }
+    } 
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+}
