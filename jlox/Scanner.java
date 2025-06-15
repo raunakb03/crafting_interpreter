c2a34bfb6c98bb31e5f230985038b7a12bff14cb
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2025-06-15 23:32:16 +0530

    parsing strings

--- jlox/Scanner.java
+++ jlox/Scanner.java
@@ -71,6 +71,7 @@ class Scanner {
             case '\n':
                        line++;
                        break;
+            case '"': string(); break;
             default:
                        Lox.error(line, "Unexpected character.");
                        break;
@@ -101,6 +102,20 @@ class Scanner {
     private char peek() {
         if(isAtEnd()) return '\0';
         return source.charAt(current);
+    }
+
+    private void string() {
+        while(peek() != '"' && !isAtEnd()) {
+            if(peek() == '\n') line++;
+            advance();
+        }
+
+        if(isAtEnd()){
+            Lox.error(line, "Unterminated string.");
+        }
+        advance();
+        String value = source.substring(start+1, current-1);
+        addToken(STRING, value);
     }
 }
 
